4386f3b525bc80b65548d7ad522c4d70db012bcf
Niclas van Eyk <niclas.eyk@gmail.com>
2020-06-09 18:42:02 +0200

    Added a script to setup a test project

---
+++ setup-local-testing-environment.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+cd ../
+rm -rf laravel-mail-in-the-middle-testing
+
+# Create a fresh laravel project
+composer create-project --prefer-dist laravel/laravel laravel-mail-in-the-middle-testing
+cd laravel-mail-in-the-middle-testing
+
+# Add our local version of mail-in-the-middle as a source, so composer installes the local version
+sed -i 's/"require": {/"repositories": [{"type": "path","url": "..\/laravel-mail-in-the-middle"}],\n    "require": {/' composer.json
+
+# Install and symlink our local version of the package into the fresh project
+composer require niclas-van-eyk/laravel-mail-in-the-middle @dev
+
+# Create and use the mailer as described in the readme
+sed -i "s/'mailers' => \[/'mailers' => \[\n\n        'mail-in-the-middle' => \[\n            'transport' => 'mail-in-the-middle',\n        ],\n/" config/mail.php
+sed -i 's/MAIL_MAILER=smtp/MAIL_MAILER=mail-in-the-middle/' .env
